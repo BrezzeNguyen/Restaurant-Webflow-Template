3d8d7fb134da85284f32e24348908aea9061907d
Brezze <brezzeNguyen1521@gmail.com>
2022-12-07 01:39:36 +0700

    done scss and responsive menu

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -16,42 +16,66 @@
 //   });
 // });
 
-
 // Show time active
 
 const iconTime = document.querySelector(".front-item-icon--time");
 const timeline = document.querySelector(".front-timeline");
 const activeClass = "is-active";
-iconTime && iconTime.addEventListener("click",function () {
+iconTime &&
+  iconTime.addEventListener("click", function () {
     timeline.classList.add(activeClass);
-});
-    
-window.addEventListener("click", function (e){
-    if (!timeline.contains(e.target) && !e.target.matches(".front-item-icon--time"))
-      timeline.classList.remove("is-active");
-    });
+  });
 
+window.addEventListener("click", function (e) {
+  if (
+    !timeline.contains(e.target) &&
+    !e.target.matches(".front-item-icon--time")
+  )
+    timeline.classList.remove("is-active");
+});
 
 const iconToggle = document.querySelector(".front-item-icon--toggle");
 const toggle = document.querySelector(".front-toggle");
-iconToggle && iconToggle.addEventListener("click",function () {
+iconToggle &&
+  iconToggle.addEventListener("click", function () {
     toggle.classList.add(activeClass);
-});
-    
-window.addEventListener("click", function (e){
-    if (!toggle.contains(e.target) && !e.target.matches(".front-item-icon--toggle"))
+  });
+
+window.addEventListener("click", function (e) {
+  if (
+    !toggle.contains(e.target) &&
+    !e.target.matches(".front-item-icon--toggle")
+  )
     toggle.classList.remove("is-active");
-    });
+});
 
 const iconCart = document.querySelector(".front-item-icon--cart");
 const Cart = document.querySelector(".front-cart");
-const background = document.getElementsByClassName(".front")
-iconCart && iconCart.addEventListener("click",function () {
+const background = document.getElementsByClassName(".front");
+iconCart &&
+  iconCart.addEventListener("click", function () {
     Cart.classList.add(activeClass);
-    background.style.backgroundColor = "red"
-});
-    
-window.addEventListener("click", function (e){
-    if (!Cart.contains(e.target) && !e.target.matches(".front-item-icon--cart"))
+    background.style.backgroundColor = "red";
+  });
+
+window.addEventListener("click", function (e) {
+  if (!Cart.contains(e.target) && !e.target.matches(".front-item-icon--cart"))
     Cart.classList.remove("is-active");
-    });
+});
+
+$(".discover").slick({
+  speed: 100,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  prevArrow: false,
+  nextArrow:false
+});
+
+// $(".discover").ready(function(){
+//     $('.slider').slick({
+//         autoplay:true,
+//         arrows: true,
+//         prevArrow:"<button type='button' class='slick-prev pull-left'><i class='fa fa-angle-left' aria-hidden='true'></i></button>",
+//         nextArrow:"<button type='button' class='slick-next pull-right'><i class='fa fa-angle-right' aria-hidden='true'></i></button>"
+//     });
+// });
